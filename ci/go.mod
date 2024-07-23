6cdeaa48060915ee203fdd3b61d008f5e12cc8db
Chris Lesiw <chris@chrislesiw.com>
2024-07-23 18:53:19 -0400

    ci: Move to labs.lesiw.io/ci/golib

--- ci/go.mod
+++ ci/go.mod
@@ -3,13 +3,13 @@ module ci.local
 go 1.22.5
 
 require (
-	labs.lesiw.io/ci v0.0.0-20240723022927-812f134ca90c
+	labs.lesiw.io/ci v0.0.0-20240723222752-b92be0823dae
 	lesiw.io/ci v0.5.0
-	lesiw.io/cmdio v0.0.0-20240721221901-62ec195983db
 )
 
 require (
 	golang.org/x/sync v0.7.0 // indirect
 	lesiw.io/clerk v0.1.0 // indirect
+	lesiw.io/cmdio v0.0.0-20240721221901-62ec195983db // indirect
 	lesiw.io/flag v0.7.0 // indirect
 )
--- ci/main.go
+++ ci/main.go
@@ -2,121 +2,19 @@ package main
 
 import (
 	"os"
-	"strings"
 
+	"labs.lesiw.io/ci/golib"
 	"lesiw.io/ci"
-	"lesiw.io/cmdio"
-	"lesiw.io/cmdio/sys"
-
-	"labs.lesiw.io/ci/golang"
 )
 
-type target struct {
-	goos   string
-	goarch string
-	unames string
-	unamer string
-}
-
-var targets = []target{
-	{"linux", "386", "linux", "i386"},
-	{"linux", "amd64", "linux", "x86_64"},
-	{"linux", "arm", "linux", "armv7l"},
-	{"linux", "arm64", "linux", "aarch64"},
-	{"darwin", "amd64", "darwin", "x86_64"},
-	{"darwin", "arm64", "darwin", "arm64"},
-	{"windows", "386", "", ""},
-	{"windows", "arm", "", ""},
-	{"windows", "amd64", "", ""},
-	{"plan9", "386", "", ""},
-	{"plan9", "arm", "", ""},
-	{"plan9", "amd64", "", ""},
-}
-
 type actions struct {
-	golang.Actions
+	golib.Actions
 }
-
-var name = "clerk"
 
 func main() {
+	golib.Name = "clerk"
 	if len(os.Args) < 2 {
 		os.Args = append(os.Args, "build")
 	}
 	ci.Handle(actions{})
-}
-
-func (a actions) Build() {
-	a.Clean()
-	a.Lint()
-	a.Test()
-	a.Race()
-	for _, t := range targets {
-		box := sys.Env(map[string]string{
-			"CGO_ENABLED": "0",
-			"GOOS":        t.goos,
-			"GOARCH":      t.goarch,
-		})
-		box.MustRun("go", "build", "-o", "/dev/null")
-	}
-}
-
-func (a actions) Clean() {
-	sys.MustRun("rm", "-rf", "out")
-	sys.MustRun("mkdir", "out")
-}
-
-func (a actions) Lint() {
-	ensureGolangci()
-	sys.MustRun("golangci-lint", "run")
-
-	sys.MustRun("go", "run", "github.com/bobg/mingo/cmd/mingo@latest", "-check")
-}
-
-func ensureGolangci() {
-	if _, err := sys.Get("which", "golangci-lint"); err == nil {
-		return
-	}
-	gopath := sys.MustGet("go", "env", "GOPATH")
-	cmdio.MustPipe(
-		sys.Command("curl", "-sSfL",
-			"https://raw.githubusercontent.com/golangci"+
-				"/golangci-lint/master/install.sh"),
-		sys.Command("sh", "-s", "--", "-b", gopath.Output+"/bin"),
-	)
-}
-
-func (a actions) Test() {
-	ensureGoTestSum()
-	sys.MustRun("gotestsum", "./...")
-}
-
-func ensureGoTestSum() {
-	if _, err := sys.Get("which", "gotestsum"); err == nil {
-		return
-	}
-	sys.MustRun("go", "install", "gotest.tools/gotestsum@latest")
-}
-
-func (a actions) Race() {
-	sys.MustRun("go", "build", "-race", "-o", "/dev/null")
-}
-
-func (a actions) Bump() {
-	bump := cmdio.MustGetPipe(
-		sys.Command("curl", "lesiw.io/bump"),
-		sys.Command("sh"),
-	).Output
-	current := "v0.0.0"
-	tag, err := sys.Get("git", "describe", "--abbrev=0", "tags")
-	if err == nil {
-		current = tag.Output
-	}
-	version := cmdio.MustGetPipe(
-		strings.NewReader(current+"\n"),
-		sys.Command(bump, "-s", "1"),
-	).Output
-	sys.MustRun("git", "tag", version)
-	sys.MustRun("git", "push")
-	sys.MustRun("git", "push", "--tags")
 }
